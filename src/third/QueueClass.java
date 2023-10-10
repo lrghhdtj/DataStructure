cd74e7beade5847cc6b23988b0706bafa401a8ba
lin <3023468046@qq.com>
2023-10-10 22:06:42 +0800

    完成作业3

---
+++ src/third/QueueClass.java
@@ -0,0 +1,114 @@
+package third;
+
+public class QueueClass <E>{
+    final int maxsize = 6;
+    private E[] data;
+    private int front,rear;
+    String tag = "false";
+    public QueueClass(){
+        data = (E[]) new Object[maxsize];
+        rear = 0;
+        front = 0;
+    }
+    public boolean empty(){
+        return rear == front;
+    }
+    public void push(E e){
+        if ((rear +1 ) % maxsize ==front){
+            tag = "true";
+            throw new IllegalArgumentException("队列已满！");
+        }else {
+            rear = (rear +1) % maxsize;
+            data[rear] = e;
+            if ((rear +1 ) % maxsize ==front){
+                tag = "true";
+            } else {
+            tag = null;
+            }
+        }
+    }
+    public E pop(){
+        if (empty()){
+            throw new IllegalArgumentException("队列为空！");
+        }else {
+            front = (front +1 ) % maxsize;
+            if (rear == front){
+                tag = "false";
+            }
+            return data[front];
+        }
+    }
+    public E peek(){
+        if (empty()){
+            throw new IllegalArgumentException("队列为空！");
+        }else {
+            return data[(front +1 ) % maxsize];
+        }
+    }
+    public int size (){
+        return ((rear - front + maxsize) % maxsize);
+    }
+
+    public boolean contain(E e){
+        int i = 0;
+       while (i < size()){
+           if (data[i] == e){
+               i++;
+               return true;
+           }else {
+               i++;
+           }
+       }
+        return false;
+    }
+    public E dataindexof(QueueClass<E> queueClass ,int index){
+        E e = null;
+        if (index < 0 || index >size() -1){
+            throw new IllegalArgumentException("参数错误！");
+        }else {
+            for (int i = 0; i < size(); i++) {
+                if ( i != index){
+                    queueClass.push(queueClass.pop());
+                }else {
+                    e = queueClass.peek();
+                    queueClass.push(queueClass.pop());
+                }
+            }
+        }
+        return e;
+    }
+
+
+    public Integer min(QueueClass<Integer> queueClass){
+        int i = 0;
+        Integer min = queueClass.pop();
+        while (i < queueClass.size()-1){
+            if (queueClass.peek() < min){
+                queueClass.push(min);
+                min = queueClass.pop();
+                queueClass.push(min);
+                i++;
+            }else {
+                queueClass.push(queueClass.pop());
+                i++;
+            }
+        }
+        return min;
+    }
+/*    public void sort (QueueClass<Integer> queueClass){
+        int i = 0;
+        Integer t = queueClass.pop(); ;
+        while (i < queueClass.size() -1 ){
+            if(t < queueClass.peek()){
+                queueClass.push(t);
+                queueClass.push(queueClass.pop());
+                i++;
+            }else {
+                queueClass.push(queueClass.peek());
+                queueClass.push(t);
+                t = queueClass.pop();
+                i++;
+            }
+        }
+    }*/
+}
---
+++ src/third/task2.java
@@ -0,0 +1,19 @@
+package third;
+
+public class task2 {
+    /*设计一个循环队列，用data[0..MaxSize-1]存放队列元素，用front和rear分别作为队头和队尾指针，
+    另外用一个标志tag标识队列可能空（false）或可能满（true），
+    这样加上front==rear可以作为队空或队满的条件。要求设计队列的相关基本运算算法。
+     */
+    public static void main(String[] args) {
+        QueueClass<Integer> queueClass = new QueueClass<>();
+        queueClass.push(11);
+        queueClass.push(22);
+        queueClass.push(3);
+        queueClass.push(46);
+        System.out.println("最小值："+queueClass.min(queueClass));
+        for (int i = 0; i < queueClass.size() ; i++) {
+            System.out.println(queueClass.dataindexof(queueClass,i));
+        }
+    }
+}
